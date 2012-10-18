57dec8dda67622973e929c55541ba69c87973ab2
Nik Torfs <niktorfs@gmail.com>
2012-10-18 23:36:26 +0200

    UI flow finally in storyboard

---
+++ iOS/QuantifyThis/QuantifyThis/RegisterMoodController.h
@@ -0,0 +1,15 @@
+//
+//  RegisterMoodController.h
+//  QuantifyThis
+//
+//  Created by Nik Torfs on 15/10/12.
+//  Copyright (c) 2012 KULeuven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterMoodController : UIViewController
+
+- (IBAction)registerMood:(id)sender;
+
+@end
