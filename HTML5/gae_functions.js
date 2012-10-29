0e135a12e5c88393cb048faab365c6da407a9a15
Michael Gobbers <michaelgobbers@gmail.com>
2012-10-29 21:47:21 +0100

    sleep values are now added to the HTTP post

--- HTML5/gae_functions.js
+++ HTML5/gae_functions.js
@@ -1,9 +1,14 @@
 function sendMood(){
+    //Mood values
     var value1  = document.getElementById("slider1").value;
     var value2  = document.getElementById("slider2").value;
     var value3  = document.getElementById("slider3").value;
     var value4  = document.getElementById("slider4").value;
     var value5  = document.getElementById("slider5").value;
     
-    $.post("http://localhost:8888/DataService", { request: "addMood", mood1: value1, mood2: value2, mood3: value3, mood4: value4, mood5: value5 } );
+    //sleep values
+    var sleepHours = document.getElementById("slider-many-hours").value;
+    var sleepEff = document.getElementById("slider-eff").value;
+    
+    $.post("http://localhost:8888/DataService", { request: "addMood", mood1: value1, mood2: value2, mood3: value3, mood4: value4, mood5: value5, sleephours: sleepHours, sleepeff: sleepEff } );
 }
