78bd86fc942633dceb192962d9360e2c5ec3c8e9
Nik Torfs <niktorfs@gmail.com>
2012-11-05 12:53:35 +0100

    adding shakecounter to count phone shakes

--- QuantifyThisAndroid/src/be/mume/quantifythis/fragments/HeartRateFragment.java
+++ QuantifyThisAndroid/src/be/mume/quantifythis/fragments/HeartRateFragment.java
@@ -7,17 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import be.mume.quantifythis.model.MarkMoodModel;
+import be.mume.quantifythis.shakeCounter.ShakeCounter;
+import be.mume.quantifythis.shakeCounter.ShakeCounterListener;
 
 /**
  * Fragment displaying the users heart rate. still needs to be implemented.
  * @author michaelgobbers
  *
  */
-public class HeartRateFragment extends Fragment {
+public class HeartRateFragment extends Fragment implements ShakeCounterListener {
     private MarkMoodModel model;
+    private ShakeCounter shakeCounter;
 
     public HeartRateFragment(MarkMoodModel model) {
         this.model = model;
+        this.shakeCounter = new ShakeCounter(getActivity());
+        this.shakeCounter.registerListener(this);
 	}
 
     @Override
@@ -26,5 +31,10 @@ public class HeartRateFragment extends Fragment {
     	View view = inflater.inflate(R.layout.fragment_heart_rate, container, false);
         
         return view;
+    }
+
+    @Override
+    public void onShake() {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
