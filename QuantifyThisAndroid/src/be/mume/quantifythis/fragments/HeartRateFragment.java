fcf093747ffcf70d6192549b8ec9726fbc185133
Nik Torfs <niktorfs@gmail.com>
2012-11-05 17:29:12 +0100

    basic heartbeat measurement working, needs some extra checks and needs to keep the screen on

--- QuantifyThisAndroid/src/be/mume/quantifythis/fragments/HeartRateFragment.java
+++ QuantifyThisAndroid/src/be/mume/quantifythis/fragments/HeartRateFragment.java
@@ -1,5 +1,10 @@
 package be.mume.quantifythis.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.PowerManager;
+import android.util.Log;
+import android.widget.TextView;
 import be.mume.quantifythis.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +15,8 @@ import be.mume.quantifythis.model.MarkMoodModel;
 import be.mume.quantifythis.shakeCounter.ShakeCounter;
 import be.mume.quantifythis.shakeCounter.ShakeCounterListener;
 
+import java.util.Date;
+
 /**
  * Fragment displaying the users heart rate. still needs to be implemented.
  * @author michaelgobbers
@@ -18,23 +25,49 @@ import be.mume.quantifythis.shakeCounter.ShakeCounterListener;
 public class HeartRateFragment extends Fragment implements ShakeCounterListener {
     private MarkMoodModel model;
     private ShakeCounter shakeCounter;
+    private PowerManager.WakeLock wakeLock;
+    private TextView bpmLabel;
+    private long startTime;
+    private int avg;
 
     public HeartRateFragment(MarkMoodModel model) {
         this.model = model;
+	}
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
         this.shakeCounter = new ShakeCounter(getActivity());
         this.shakeCounter.registerListener(this);
-	}
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
     	View view = inflater.inflate(R.layout.fragment_heart_rate, container, false);
-        
+        bpmLabel = (TextView) view.findViewById(R.id.bpm_label);
         return view;
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        this.shakeCounter.startListening();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.shakeCounter.stopListening();
+    }
+
+    @Override
     public void onShake() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if(shakeCounter.getTotalCount() == 1){
+            startTime = new Date().getTime();
+        }else{
+            this.avg = (int) ((60000 * shakeCounter.getTotalCount()) / (new Date().getTime() - startTime));
+            bpmLabel.setText(avg + "");
+        }
     }
 }
