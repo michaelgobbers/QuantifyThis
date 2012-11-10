9240ff81cfa9d57d4a98ee104f1dfa93574550ab
Michael Gobbers <michaelgobbers@gmail.com>
2012-11-10 14:43:34 +0100

    added categories and a list of events depending on the selected categories.

--- HTML5/calendar_api.js
+++ HTML5/calendar_api.js
@@ -2,6 +2,7 @@ var clientId = '784611597419.apps.googleusercontent.com';
 var apiKey = 'AIzaSyBlFAus1q27yw2zO9v9R5dhdS-f1LayZg4';
 var scopes = 'https://www.googleapis.com/auth/calendar';
 var apicallmade =false;
+var calendarAmount = 0;
 
 function loadCalendar() {
   gapi.client.setApiKey(apiKey);
@@ -21,7 +22,7 @@ function handleAuthResultCalendar(authResult) {
   if (authResult) {
     authorizeButton.style.visibility = 'hidden';
     if(!apicallmade){
-        makeApiCallCalendar();
+        displayCategories();
         apicallmade=true;
     }
   } else {
@@ -36,20 +37,62 @@ function handleAuthClickCalendar(event) {
       handleAuthResultCalendar);
   return false;
 }
+function setEventDetailsInMarkMood(id, summary){
+    var markmoodeventsummary = $("#markevent");
+    var marmoodeventid = $("#markeventid");
+    markmoodeventsummary.text(summary);
+    marmoodeventid.val(id);
+}
+function showCalendarsWithID(ids, list) {
+    gapi.client.load('calendar', 'v3', function() {
+        for(var i=0;i<ids.length;i++){
+            var request = gapi.client.calendar.events.list({
+                    'calendarId': ids[i]
+            });
+                  
+            request.execute(function(resp, i) {
+                for (var j = 0; j < resp.items.length; j++) {
+                    var html = '<li onclick="setEventDetailsInMarkMood(\''+resp.items[j].id+'\',\''+ resp.items[j].summary +'\')"><a href="#MarkMyMood">'+resp.items[j].summary+'</a></li>';           
+                    $(list).append(html);
+                }
+                $(list).listview("refresh");
+            });
+        }
+    });
+    
+}
 
-function makeApiCallCalendar() {
-  gapi.client.load('calendar', 'v3', function() {
-    var request = gapi.client.calendar.events.list({
-      'calendarId': 'primary'
+function showCalendars(){
+    var ids = new Array();
+    var index = 0;
+    for(var i = 0 ; i<calendarAmount;i++){
+        var checkbox = $('#checkbox-'+i);
+        var checked = $(checkbox).is(":checked");
+        if(checked){
+            ids[index] = $(checkbox).val();
+            index++;
+        }
+    }
+    $('#events-list').empty();
+    showCalendarsWithID(ids, $('#events-list'));
+}
+
+function displayCategories(){
+    gapi.client.load('calendar', 'v3', function() {
+    var request = gapi.client.calendar.calendarList.list({
     });
           
     request.execute(function(resp) {
+        var html = '<fieldset data-role="controlgroup" data-type="horizontal" data-role="fieldcontain" id="categories">';
+        
+        calendarAmount = resp.items.length;
       for (var i = 0; i < resp.items.length; i++) {
-        var li = document.createElement('li');
-        li.id="item";
-        li.appendChild(document.createTextNode(resp.items[i].summary));
-        document.getElementById('step').appendChild(li);
+        
+        html = html+'<input value="'+ resp.items[i].id +'" onclick="showCalendars()" type="checkbox" name="checkbox-'+i+'" id="checkbox-'+i+'" class="custom" /><label for="checkbox-'+i+'">'+ resp.items[i].summary +'</label>';
       }
+      html = html + '</fieldset>';
+      $('#categories-container').append(html).trigger('create');
+      
     });
   });
 }
